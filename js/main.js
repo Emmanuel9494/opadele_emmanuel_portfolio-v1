23f8345b8068d177490de761e7f1e0d9264f007b
Emmanuel9494 <144351016+Emmanuel9494@users.noreply.github.com>
2024-09-16 21:03:25 -0400

    Sass Folder And Custom Video Controls
    Created a Sass Folder, moved CSS to .scss, compiler active, added some more custom video player features

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ console.log("JS is Running, Try Catching It")
 // (1.) Mobile Menu Icon Toggle//////////
 let Menubutton = document.querySelector(".container-mobile-top-logo");
 // (2.) Globe Icon Switch On Hover
-const globeIcon = document.getElementById('globe-icon');
+const globeIcon = document.querySelector('#globe-icon');
 const originalSrc = 'images/icon-globe.svg';
 const hoverSrc = 'images/icon-red-globe.svg';
 // (3. ) Custom Video Variables
@@ -20,9 +20,13 @@ const videoControls = document.querySelector('#video-controls');
 const playerButton = document.querySelector('#play-button');
 const pauseButton = document.querySelector('#pause-button');
 const stopButton = document.querySelector('#stop-button');
+const volumeSlider = document.querySelector('#change-vol');
+const fullScreen = document.querySelector('#full-screen');
+const videoContainer = document.querySelector('#reel-box');
+const soundButton = document.querySelector('#volume');
 // 4. // video Count Down 
-const video = document.getElementById('video-player');
-const countdownText = document.getElementById('countdown-text');
+const video = document.querySelector('#video-player');
+const countdownText = document.querySelector('#countdown-text');
 let countdownInterval;
 let timeRemaining = 0;
 
@@ -78,8 +82,64 @@ function stopVideo(){
     countdownText.textContent = timeRemaining;
 }
 
-function screenPause(){
-player.pause();
+function screenPauseEtPlay(){
+    if (player.paused) {
+        player.play();  // If video is paused, play it
+        startCountdown(); // Start the countdown
+      } else {
+        player.pause();  // If video is playing, pause it
+        clearInterval(countdownInterval); // Stop countdown when the video ends
+      }
+}
+function changeVolume(){
+    console.log(volumeSlider.value); 
+    console.log('Volume changing');
+    player.volume = volumeSlider.value;
+    if (volumeSlider.value == 0) {
+        player.muted = true; // If slider is set to 0, mute the video
+        soundButton.src = 'images/icon-volume-mute.svg'; // Change to muted icon
+    } else {
+        player.muted = false; // Unmute if slider changes to non-zero
+        player.volume = volumeSlider.value; // Set video volume to slider value
+        soundButton.src = 'images/ctrl-volume.svg'; // Change back to unmuted icon
+    }
+}
+function toggleFullScreen(){
+    console.log('Full Screen Toggled');
+    if(document.fullscreenElement){
+        document.exitFullscreen();
+    } else if(document.webkitFullScreenElement){
+        document.webkitexitFullscreen();
+
+    } else if(videoContainer.webkitRequestFullscreen){
+        videoContainer.webkitRequestFullscreen();
+    } else {
+        videoContainer.requestFullscreen();
+    }
+}
+function showControls(){
+    console.log('video controls hidden');
+    videoControls.classList.remove('hide');
+}
+function hideControls(){
+    if(player.paused){
+        return;
+    }
+    videoControls.classList.add('hide');
+}
+function muteOrUnmute() {
+    
+    if (player.muted) {
+        player.muted = false; // Unmute the video
+        volumeSlider.value = player.volume; // Update slider to current volume
+        soundButton.src = 'images/ctrl-volume.svg'; 
+        console.log('Sound On');
+    } else {
+        player.muted = true; // Mute the video
+        volumeSlider.value = 0; // Set slider to 0
+        soundButton.src = 'images/icon-volume-mute.svg'; 
+        console.log('Sound Off');
+    }
 }
 // 4. // video Count Down
 // Countdown timer
@@ -112,7 +172,14 @@ globeIcon.addEventListener('mouseout', resetGlobeIcon);
 playerButton.addEventListener("click", playVideo);
 pauseButton.addEventListener("click", pauseVideo);
 stopButton.addEventListener("click", stopVideo);
-player.addEventListener("click", screenPause);
+player.addEventListener("click", screenPauseEtPlay);
+volumeSlider.addEventListener("change", changeVolume);
+fullScreen.addEventListener("click", toggleFullScreen);
+videoControls.addEventListener("mouseenter", showControls);
+videoControls.addEventListener("mouseleave", hideControls);
+player.addEventListener("mouseenter", showControls);
+player.addEventListener("mouseleave", hideControls);
+soundButton.addEventListener("click", muteOrUnmute);
 
 
 
